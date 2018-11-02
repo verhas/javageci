6cf72cce99f2a58204355292f7e75e66286e3f1c
Peter_Verhas <peter@verhas.com>
2018-11-02 17:45:40 +0100

    test modified a bit to be more expressive

--- javageci-jamal/src/test/java/javax0/geci/jamal/PreprocessedFile.java
+++ javageci-jamal/src/test/java/javax0/geci/jamal/PreprocessedFile.java
@@ -4,9 +4,13 @@ public class PreprocessedFile {
     public void dummy(){
 
     /*!jamal
+    //<editor-fold desc="the generated code">
     {{@define z=13}}var i = {{z}};
+    //</editor-fold>
      */
+    //<editor-fold desc="the generated code">
     var i = 13;
+    //</editor-fold>
     //__END__
     }
 }
