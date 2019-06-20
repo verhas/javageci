8e62088ff28da6c0b7d7cd8d72cf907c48955759
Peter_Verhas <peter@verhas.com>
2019-06-20 09:42:26 +0200

    Java source code comparator first does a fast check before tokenizing

--- javageci-tools/src/main/java/javax0/geci/javacomparator/Comparator.java
+++ javageci-tools/src/main/java/javax0/geci/javacomparator/Comparator.java
@@ -5,27 +5,37 @@ import javax0.geci.javacomparator.lex.LexicalElement;
 
 import java.util.List;
 import java.util.function.BiPredicate;
+import java.util.stream.IntStream;
 
 public class Comparator implements BiPredicate<List<String>, List<String>> {
     /**
      * Evaluates this predicate on the given arguments.
      *
-     * @param strings1  the first input argument
+     * @param strings1 the first input argument
      * @param strings2 the second input argument
      * @return {@code true} if the input arguments match the predicate,
      * otherwise {@code false}
      */
     @Override
     public boolean test(List<String> strings1, List<String> strings2) {
+        if (fastCheck(strings1, strings2)) return false;
         Lexer lexer = new Lexer();
         LexicalElement[] elements1 = lexer.apply(strings1);
         LexicalElement[] elements2 = lexer.apply(strings2);
-        if( elements1.length != elements2.length ){return true;}
-        for( int i = 0 ; i < elements1.length ; i ++){
-            if( !elements1[i].equals(elements2[i])){
+        if (elements1.length != elements2.length) {
+            return true;
+        }
+        for (int i = 0; i < elements1.length; i++) {
+            if (!elements1[i].equals(elements2[i])) {
                 return true;
             }
         }
         return false;
+    }
+
+    private boolean fastCheck(List<String> strings1, List<String> strings2) {
+        return strings1.size() == strings2.size() &&
+                !IntStream.range(0, strings2.size())
+                        .anyMatch(i -> !strings2.get(i).equals(strings1.get(i)));
     }
 }
