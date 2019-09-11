ee1901b7f876152d2e9cd1e4e941a59a05937cfc
Peter Verhas <peter@verhas.com>
2019-09-11 10:52:33 +0200

    JavaDoc segment calculates the tabbing now correctly more JavaDoc added getting examples from unit test

--- javageci-docugen/src/main/java/javax0/geci/docugen/JavaDocSegmentSplitHelper.java
+++ javageci-docugen/src/main/java/javax0/geci/docugen/JavaDocSegmentSplitHelper.java
@@ -1,21 +1,14 @@
 package javax0.geci.docugen;
 
-import javax0.geci.api.SegmentSplitHelper;
-
-import java.util.List;
 import java.util.regex.Pattern;
 
 public class JavaDocSegmentSplitHelper extends AbstractXMLSegmentSplitHelper {
     public JavaDocSegmentSplitHelper() {
         super(
             Pattern.compile(
-                    "^(\\s*)\\*?\\s*<!--\\s*snip\\s+(.*)-->\\s*$"
+                "^(\\s*)\\*?\\s*<!--\\s*snip\\s+(.*)-->\\s*$"
             ),
             Pattern.compile(
-                "^(\\s*)```(\\s*)$" +
-                    "|" +
-                    "^(\\s*)\\[//]:\\s*#\\s*\\(\\s*end\\s+snip\\s*(.*)\\)\\s*$" +
-                    "|" +
                     "^(\\s*)\\*?\\s*<!--\\s*end\\s+snip\\s*(.*)-->\\s*$"
             ),
             Pattern.compile("DEFAULT SEGMENT") // probably will not match ever, if yes, c'mon!
@@ -24,10 +17,10 @@ public class JavaDocSegmentSplitHelper extends AbstractXMLSegmentSplitHelper {
     }
 
     protected String stripLine(String line) {
-        return line.stripLeading().replaceAll("^\\*","");
+        return line.stripLeading().replaceAll("^\\*\\s*", "");
     }
 
     protected String stripContinuationLine(String line) {
-        return line.replaceAll("^\\s*\\*","");
+        return line.replaceAll("^(\\s*)\\*\\s*", "$1");
     }
 }
