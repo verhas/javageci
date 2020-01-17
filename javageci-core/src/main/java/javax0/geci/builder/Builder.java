ed302f77983ac0748fbfed0b8a8b13d61082e9cd
Mihaly Verhas <mihaly_verhas@epam.com>
2020-01-17 15:54:18 +0100

    Minor style fixes.

--- javageci-core/src/main/java/javax0/geci/builder/Builder.java
+++ javageci-core/src/main/java/javax0/geci/builder/Builder.java
@@ -220,9 +220,9 @@ public class Builder extends AbstractFilteredFieldsGenerator {
         writeGenerated(segment, config.generatedAnnotation);
         segment.write_r("public static %s.%s %s() {", klass.getSimpleName(), local.builderName, local.builderFactoryMethod)
                 .write("return %s.new %s();", factory, local.builderName)
-                .write_l("}")
-                .newline()
-                .write_r("public class %s {", local.builderName);
+                .write_l("}");
+        writeGenerated(segment, config.generatedAnnotation);
+        segment.write_r("public class %s {", local.builderName);
     }
 
     @Override
