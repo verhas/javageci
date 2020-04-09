0ab0f91196e97481d1ac3f27cfdd0197df2f2013
Peter Verhas <peter@verhas.com>
2020-04-09 10:35:08 +0200

    jdocify added to POM to be opened for test reflection jdocify developer further to care about new line and star character in the code part GeneratorTester was added to the engine module in testsupport package TestJdocify uses the GeneratorTester Source mock support stores the originals so it is possible to check with isModified Source mock support has proxy methods to isTouched and isModified so it can be checked in tests code gets touched when it is modified (or not) from lexical analysis (borrows and returns)

--- javageci-core/src/test/java/javax0/geci/jdocify/TestJdocify.java
+++ javageci-core/src/test/java/javax0/geci/jdocify/TestJdocify.java
@@ -1,55 +1,82 @@
 package javax0.geci.jdocify;
 
-import javax0.geci.engine.Source;
-import javax0.geci.tools.CompoundParams;
-import javax0.geci.tools.CompoundParamsBuilder;
+import javax0.geci.engine.testsupport.GeneratorTester;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 public class TestJdocify {
+    // used reflective by tested code
     final static String REPLACE = "replaced indeed";
 
-    void generalTest(final List<String> sourceLines, final List<String> expectedOutput) throws Exception {
-        // GIVEN
-        final var sut = Jdocify.builder().build();
-        final var source = Source.mock(sut).lines(sourceLines.stream().toArray(String[]::new)).getSource();
-        final CompoundParams global = new CompoundParamsBuilder("jdocify").build();
-        // WHEN
-        sut.process(source, TestJdocify.class, global);
+    private static void check(GeneratorTester tester) throws Exception {
+        tester.test();
+        Assertions.assertEquals(tester.expected(), tester.actual());
+    }
 
-        // THEN
-        source.consolidate();
-        Assertions.assertEquals(String.join("\n", expectedOutput),
-            String.join("\n", source.getLines()));
+    private GeneratorTester generator() {
+        return GeneratorTester.generator(Jdocify.builder().build()).klass(TestJdocify.class);
     }
 
     @Test
+    @DisplayName("Simple CODE replacement without template works")
     void simpleReplace() throws Exception {
-        generalTest(
-            List.of(
+        check(generator()
+            .source(
                 "/**",
                 " * This is to <!--CODE REPLACE-->{@code this is to be replaced} be replaced",
-                "*/"
-            ),
-            List.of(
-                "/**",
+                "*/")
+            .expected("/**",
                 " * This is to <!--CODE REPLACE-->{@code replaced indeed} be replaced",
+                "*/")
+        );
+    }
+
+    @Test
+    @DisplayName("Multi line CODE replacement without template works")
+    void multiReplace() throws Exception {
+        check(generator()
+            .source(
+                "/**",
+                " * This is to <!--CODE",
+                " * REPLACE-->{@code this is ",
+                " * to be replaced} be replaced",
+                "*/"
+            )
+            .expected(
+                "/**",
+                " * This is to <!--CODE",
+                " * REPLACE-->{@code replaced indeed} be replaced",
                 "*/"
             )
         );
     }
 
     @Test
+    @DisplayName("Multi line CODE replacement is not hurt if only reformatted")
+    void multiReplaceNon() throws Exception {
+        check(generator()
+            .source(
+                "/**",
+                " * This is to <!--CODE",
+                " * REPLACE-->{@code replaced",
+                " * indeed} be replaced",
+                "*/"
+            )
+            .noChange()
+        );
+    }
+
+    @Test
+    @DisplayName("Simple CODE replacement with template works")
     void templatedReplace() throws Exception {
-        generalTest(
-            List.of(
+        check(generator()
+            .source(
                 "/**",
                 " * This is to <!--CODE REPLACE $REPLACE x-->{@code this is to be replaced} be replaced",
                 "*/"
-            ),
-            List.of(
+            )
+            .expected(
                 "/**",
                 " * This is to <!--CODE REPLACE $REPLACE x-->{@code $replaced indeed x} be replaced",
                 "*/"
