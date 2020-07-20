4b264389cbae5600e947c63fd82044bdcae193b0
Peter Verhas <peter@verhas.com>
2020-07-20 14:29:23 +0200

    extra </p> was removed, it broke the build with Java 11 on Windows

--- javageci-tools/src/main/java/javax0/geci/log/package-info.java
+++ javageci-tools/src/main/java/javax0/geci/log/package-info.java
@@ -5,7 +5,7 @@
  * code is compiled using the JBEL annotation processor the Java compiler will generate Java 8 compatible byte code from
  * the Java 11 source code.</p>
  *
- * <p></p>The only exception where we did not want to lose the Java 9+ feature is the Java 9+ logging. To have a
+ * <p>The only exception where we did not want to lose the Java 9+ feature is the Java 9+ logging. To have a
  * flexible logging that can use Java 9+ logging possibilities when available we use reflection when the {@link
  * javax0.geci.log.Logger} class is loaded.</p>
  */
